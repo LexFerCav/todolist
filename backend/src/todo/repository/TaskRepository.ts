4dad3b6316e1b148aa7949adf17ab2ba8c51feeb
LexFerCav <lex.fercav@gmail.com>
2024-10-24 17:35:39 -0300

    feat: todo routes done

--- backend/src/todo/repository/TaskRepository.ts
+++ backend/src/todo/repository/TaskRepository.ts
@@ -1,20 +1,33 @@
 import mongoose from "mongoose";
 import { TaskModel } from "../models/TaskModels";
+import { Task } from "../../types/task";
 
 export class TaskRepository {
   async getAllTasks() {
-    const data = await TaskModel.find().exec();
+    const data = await TaskModel.find({ deleted: { $ne: true } }).exec();
     return { data };
   }
-  async createTask(task: any) {
+  async getDeletedTasks() {
+    const data = await TaskModel.find({ deleted: { $eq: true } }).exec();
+    return { data };
+  }
+  async createTask(task: Task) {
     const data = await TaskModel.create(task);
     return { data };
   }
-  async updateTask({ _id, task }: any) {
+  async updateTask({ _id, task }: { _id: string; task: Task }) {
     const data = await TaskModel.findOneAndUpdate({ _id }, task, { new: true });
     return { data };
   }
-  async deleteTask(_id: any) {
+  async deleteTask(_id: string) {
+    const data = await TaskModel.findOneAndUpdate(
+      { _id },
+      { deleted: true },
+      { new: true }
+    );
+    return { data };
+  }
+  async hardDeleteTask(_id: string) {
     const data = await TaskModel.findByIdAndDelete({ _id });
     return { data };
   }
